6d2a16a00077edc88983b25ec1b8afae9783c53d
qliquiz <artgor005@gmail.com>
2024-03-11 21:49:50 +0800

    report

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from Logics.report_factory import report_factory
+from Logics.start_factory import start_factory
+from settings_manager import settings_manager
 
 app = Flask(__name__)
 
 @app.route('/api/report/<storage_key>', methods = ['GET'])
 def get_report(key: str):
-    response_data = app.response_class(response = f'ключ {key}', status = 200, mimetype = 'application/text')
+    manager = settings_manager()
+    start = start_factory(manager.settings)
+    factory = report_factory()
+    report = factory.create(manager.settings.report_format, start.storage)
+
+    if key not in start.storage.data.keys():
+        return "Такого ключа нет", 500
+
+    data = report.create(key)
+
+    response_data = app.response_class(response = data, status = 200, mimetype = 'application/text')
 
     return response_data
 
